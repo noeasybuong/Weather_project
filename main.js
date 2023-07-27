1944d9dde76403d6d071a9dcf25aa5ae72bc77bd
noeasybuong <tangduchoang34@gmail.com>
2023-07-27 16:31:30 +0700

    commit

--- main.js
+++ main.js
@@ -1,38 +1,71 @@
 $(document).ready(function () {
-  $(".search").click(function () {
-    let severAPI =
-      "https://api.tomorrow.io/v4/weather/realtime?location= &apikey=dkAhiJ8UZZoRL2R1cna6jgs2SwA2e1l8";
-    let dataUser = $(".inputLocation").val();
-    let weatherApi = severAPI.replace(" ", dataUser);
 
-    $.get(weatherApi, function (datas, status) {
-      const isTemperature = datas.data.values.temperature;
-      const isHumidity = datas.data.values.humidity;
-      const isPrecipitationProbability =
-        datas.data.values.precipitationProbability;
-      const isCloudCover = datas.data.values.cloudCover;
+  // window.onload = function () {
+  //   $.get("https://api.tomorrow.io/v4/weather/realtime?location=hanoi&apikey=Mxg1qyFAjPwkATUsXh6o5qb8Vx8ejY8q", function (datas, status) {
+  //     isReasultWeather();
+  //   })
+  // }
 
-      $(".weather-temperature").html(function () {
-        return "Nhiệt độ " + isTemperature + "°C";
-      });
 
-      $(".weather-humidity").html(function () {
-        return "Độ ẩm " + isHumidity + "%";
-      });
+  //onclick Enter
+  $('.inputLocation').keypress(function (event) {
+    event.preventDefault();
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+      isReasult();
+    }
 
-      $(".weather-precipitationProbability").html(function () {
-        return "Khả năng mưa " + isPrecipitationProbability + "%";
-      });
+    $(".search").click(function () {
+      isReasult();
+    }
+    )
 
-      $(".wether-cloudCover").html(function () {
-        return "Mây che phủ " + isCloudCover + "%";
-      });
 
-      if (isTemperature >30) {
-        $(".app").html(function () {
-          $(".app").css({"background-image":"url(chup-anh-khi-troi-nang-gat.jpg)"})
-        });
-      }
-    });
   });
+
 });
+
+
+//Logic
+function isReasult() {
+  let severAPI =
+    "https://api.tomorrow.io/v4/weather/realtime?location= &apikey=Mxg1qyFAjPwkATUsXh6o5qb8Vx8ejY8q";
+
+  let dataUser = $(".inputLocation").val();
+  let weatherApi = severAPI.replace(" ", dataUser);
+
+  $.get(weatherApi, function (datas, status) {
+
+
+
+    const isTemperature = datas.data.values.temperature;
+    const isHumidity = datas.data.values.humidity;
+    const isPrecipitationProbability =
+      datas.data.values.precipitationProbability;
+    const isCloudCover = datas.data.values.cloudCover;
+
+    $(".weather-temperature").html(function () {
+      return "Nhiệt độ " + isTemperature + "°C";
+    });
+
+    $(".weather-humidity").html(function () {
+      return "Độ ẩm " + isHumidity + "%";
+    });
+
+    $(".weather-precipitationProbability").html(function () {
+      return "Khả năng mưa " + isPrecipitationProbability + "%";
+    });
+
+    $(".wether-cloudCover").html(function () {
+      return "Mây che phủ " + isCloudCover + "%";
+    });
+
+    if (isTemperature > 30) {
+      $(".app").html(function () {
+        $(".app").css({ "background-image": "url(chup-anh-khi-troi-nang-gat.jpg)" })
+      });
+    }
+  });
+
+}
+
