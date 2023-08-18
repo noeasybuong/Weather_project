e7c1ab6dd13c28a9bfd35a7e2496b70b8dfc02df
noeasybuong <tangduchoang34@gmail.com>
2023-08-18 09:40:05 +0700

    commit

--- main.js
+++ main.js
@@ -1,71 +1,151 @@
 $(document).ready(function () {
 
-  // window.onload = function () {
-  //   $.get("https://api.tomorrow.io/v4/weather/realtime?location=hanoi&apikey=Mxg1qyFAjPwkATUsXh6o5qb8Vx8ejY8q", function (datas, status) {
-  //     isReasultWeather();
-  //   })
-  // }
-
+  //Onload
+  $(document).ready(function () {
+    isReasultDefault();
+  });
 
   //onclick Enter
-  $('.inputLocation').keypress(function (event) {
-    event.preventDefault();
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if (keycode == '13') {
+  $(".inputLocation").keypress(function (event) {
+    var keycode = event.keyCode ? event.keyCode : event.which;
+
+    if (keycode == "13") {
+      event.preventDefault();
       isReasult();
     }
 
     $(".search").click(function () {
       isReasult();
-    }
-    )
-
-
+    });
   });
-
 });
 
+function isReasultDefault() {
+  const defaultSeverAPI =
+    "https://api.tomorrow.io/v4/weather/realtime?location=hanoi&apikey=ermFk4uKhKdg8UgK0BbWaFkbdkUNHf5O";
+  $.get(defaultSeverAPI, function (datas, status) {
+    isReasultWeather(datas);
+  });
+}
 
 //Logic
 function isReasult() {
   let severAPI =
-    "https://api.tomorrow.io/v4/weather/realtime?location= &apikey=Mxg1qyFAjPwkATUsXh6o5qb8Vx8ejY8q";
+    "https://api.tomorrow.io/v4/weather/realtime?location= &apikey=ermFk4uKhKdg8UgK0BbWaFkbdkUNHf5O";
 
   let dataUser = $(".inputLocation").val();
   let weatherApi = severAPI.replace(" ", dataUser);
 
   $.get(weatherApi, function (datas, status) {
+    isReasultWeather(datas);
+  });
+}
 
+function isReasultWeather(values) {
+  const isLocation = values.location.name;
+  const isDate = new Date();
+  const isHours = isDate.getHours();
+  const isToday = isHours + ":" + isDate.getMinutes() + ":" + isDate.getSeconds() + "        " + isDate.getDate() + "-" + (isDate.getMonth() + 1) + "-" + isDate.getFullYear();
+  const isTemperature = values.data.values.temperature;
+  const isHumidity = values.data.values.humidity;
+  const isPrecipitationProbability =
+    values.data.values.precipitationProbability;
+  const isCloudCover = values.data.values.cloudCover;
+  const isuvIndex = values.data.values.uvIndex
 
-
-    const isTemperature = datas.data.values.temperature;
-    const isHumidity = datas.data.values.humidity;
-    const isPrecipitationProbability =
-      datas.data.values.precipitationProbability;
-    const isCloudCover = datas.data.values.cloudCover;
-
-    $(".weather-temperature").html(function () {
-      return "Nhiệt độ " + isTemperature + "°C";
-    });
-
-    $(".weather-humidity").html(function () {
-      return "Độ ẩm " + isHumidity + "%";
-    });
-
-    $(".weather-precipitationProbability").html(function () {
-      return "Khả năng mưa " + isPrecipitationProbability + "%";
-    });
-
-    $(".wether-cloudCover").html(function () {
-      return "Mây che phủ " + isCloudCover + "%";
-    });
-
-    if (isTemperature > 30) {
-      $(".app").html(function () {
-        $(".app").css({ "background-image": "url(chup-anh-khi-troi-nang-gat.jpg)" })
-      });
-    }
+  $(".icon-uvIndex").attr('src', 'images/istockphoto-968115898-170667a.jpg');
+  $(".icon-humidity").attr('src', 'images/pngtree-humidity-water-vapor-droplet-moisture-png-image_4812649.png');
+  $(".icon-precipitationProbability").attr('src', 'images/weather-forecast.png');
+  $(".icon-cloudCover").attr('src', 'images/cloudy-day.png');
+  $(".location").html(function () {
+    return isLocation;
   });
 
+  $(".todayDate").html(function () {
+    return isToday
+  });
+
+  $(".weather-temperature").html(function () {
+    return isTemperature + "°C";
+  });
+
+
+
+  $(".weather-uvIndex").html(function () {
+    return "Chỉ số UV :" + isuvIndex;
+  });
+
+  $(".weather-humidity").html(function () {
+    return "Độ ẩm " + isHumidity + "%";
+  });
+
+  $(".weather-precipitationProbability").html(function () {
+    return "Khả năng mưa " + isPrecipitationProbability + "%";
+  });
+
+  $(".wether-cloudCover").html(function () {
+    return "Mây che phủ " + isCloudCover + "%";
+  });
+
+
+
+
+
+
+
+
+
+
+
+  if (isHours >= 19 || isHours <= 4) {
+    $(".app").html(function () {
+      $(".app").css({
+        "background-image": "url(images/pexels-miriam-espacio-365633.jpg)"
+      });
+      $(".icon-weather").attr('src', 'images/moon (1).png');
+
+    });
+
+  }
+
+  else {
+
+    if (isTemperature < 34 && (isHumidity < 90 && isHumidity > 30)) {
+
+      $(".app").html(function () {
+        $(".app").css({
+          "background-image": "url(images/04099_dataibaysunset_1920x1080.jpg)"
+        });
+        $(".icon-weather").attr('src', 'images/cloudy (1).png');
+      });
+    }
+
+    if (isTemperature > 34 && isHumidity < 30) {
+
+      $(".app").html(function () {
+        $(".app").css({
+          "background-image": "url(images/04099_dataibaysunset_1920x1080.jpg)"
+        });
+
+        $(".icon-weather").attr('src', 'images/sun.png');
+      });
+    }
+
+
+    if (isTemperature < 34 && isHumidity > 90) {
+
+      $(".app").html(function () {
+        $(".app").css({
+          "background-image": "url(anh-bau-troi-am-u-dep-nhat_022454740.jpg)"
+        });
+        $(".icon-weather").attr('src', 'images/cloudy.png');
+      });
+    }
+
+  }
+
+
+
 }
+
 
